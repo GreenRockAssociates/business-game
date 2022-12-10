66677e6c220d737f75384d998e2ad5c93ee2c853
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-10 11:51:59 +0100

    Added a middleware to parse req.body from object literal to a DTO

--- Gateway/Authentication/src/dto/register-user.dto.ts
+++ Gateway/Authentication/src/dto/register-user.dto.ts
@@ -1,26 +1,35 @@
 import {IsEmail, IsNotEmpty, IsString, MinLength} from "class-validator";
-import {Trim} from "class-sanitizer";
+import {NormalizeEmail, Trim} from "class-sanitizer";
+import {Expose} from "class-transformer";
 
+/**
+ * Data tranfer object for the register HTTP request
+ */
 export class RegisterUserDto {
     @IsNotEmpty()
     @IsString()
     @Trim()
+    @Expose()
     firstName: string;
 
     @IsNotEmpty()
     @IsString()
     @Trim()
+    @Expose()
     lastName: string;
 
     @IsNotEmpty()
     @IsString()
     @IsEmail({}, {message: "Provided Email is not valid"})
     @Trim()
+    @NormalizeEmail(true)
+    @Expose()
     email: string;
 
     @IsNotEmpty()
     @IsString()
     @MinLength(12, {message: "Password should be minimum of 12 characters"})
+    @Expose()
     password: string;
 
 
---
+++ Gateway/Authentication/src/specs/middlewares/json-to-dto.middleware.spec.ts
@@ -0,0 +1,94 @@
+import {jsonToDtoMiddlewareFactory} from "../../api/middlewares/json-to-dto.middleware";
+import {RegisterUserDto} from "../../dto/register-user.dto";
+import { Request, Response, NextFunction } from 'express';
+
+describe('jsonToDtoMiddlewareFactory', () => {
+    const response = {
+        sendStatus() {}
+    };
+    let responseSpy: jest.SpyInstance;
+    let nextSpy: jest.Mock;
+
+    beforeEach(() => {
+        responseSpy = jest.spyOn(response, 'sendStatus');
+        nextSpy = jest.fn();
+    })
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    })
+
+    it('The middleware should parses object literal to class instance', async () => {
+        const request = {
+            body : {
+                firstName: "Foo",
+                lastName: "Bar",
+                email: "foo@bar.com",
+                password: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            }
+        }
+
+        const middleware = jsonToDtoMiddlewareFactory(RegisterUserDto);
+        await middleware(request as Request, response as unknown as Response, nextSpy as NextFunction);
+
+        expect(request.body instanceof RegisterUserDto).toEqual(true);
+    })
+
+    it('The middleware should sanitize the inputs', async () => {
+        const request = {
+            body : {
+                firstName: "  Foo  ",
+                lastName: "  Bar  ",
+                email: "fOo@bAr.com",
+                password: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            }
+        }
+
+        const expected = {
+            firstName: "Foo",
+            lastName: "Bar",
+            email: "foo@bar.com",
+            password: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        }
+
+        const middleware = jsonToDtoMiddlewareFactory(RegisterUserDto);
+        await middleware(request as Request, response as unknown as Response, nextSpy as NextFunction);
+
+        expect(request.body).toEqual(expected);
+    })
+
+    it('The middleware should call the next() function if the object is valid', async () => {
+        const request = {
+            body : {
+                firstName: "Foo",
+                lastName: "Bar",
+                email: "foo@bar.com",
+                password: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            }
+        }
+
+        const middleware = jsonToDtoMiddlewareFactory(RegisterUserDto);
+        await middleware(request as Request, response as unknown as Response, nextSpy as NextFunction);
+
+        expect(nextSpy).toHaveBeenCalled();
+        expect(responseSpy).not.toHaveBeenCalled();
+    })
+
+    it('The middleware should call the sendStatus() function with error code 400 if the object is invalid', async () => {
+        // Only check one kind of invalidity ; each dto should have its own tests for every kind of invalidity it has
+        const request = {
+            body : {
+                firstName: "Foo",
+                lastName: "Bar",
+                email: "foo@bar.com",
+                password: ""
+            }
+        }
+
+        const middleware = jsonToDtoMiddlewareFactory(RegisterUserDto);
+        await middleware(request as Request, response as unknown as Response, nextSpy as NextFunction);
+
+        expect(nextSpy).not.toHaveBeenCalled();
+        expect(responseSpy).toHaveBeenCalledWith(400);
+    })
+})
