62e4fafaa0c284e2c95ca2f6b0f6d0492d2cd014
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-11 16:07:52 +0100

    Added login route to express router, as well as redis session store

--- Gateway/Authentication/src/api/api.ts
+++ Gateway/Authentication/src/api/api.ts
@@ -1,12 +1,18 @@
 import express, {Router} from 'express';
-import {jsonToDtoMiddlewareFactory} from "./middlewares/json-to-dto.middleware";
 import {UserEntity} from "../entities/user.entity";
-import {registerUser} from "./routes/register.route";
-import {DataSource} from "typeorm";
+import {DataSource, Repository} from "typeorm";
 import {RegisterUserDto} from "../dto/register-user.dto";
+import {LoginDto} from "../dto/login.dto";
+
+import {jsonToDtoMiddlewareFactory} from "./middlewares/json-to-dto.middleware";
+import {registerUser} from "./routes/register.route";
+import {login} from "./routes/login.route";
 
 export const router = express.Router()
 
 export function registerRoutes(router: Router, dataSource: DataSource){
-    router.post('/register', jsonToDtoMiddlewareFactory(RegisterUserDto), (req, res) => registerUser(req, res, dataSource.getRepository(UserEntity.name)))
+    const userRepository : Repository<UserEntity> = dataSource.getRepository(UserEntity.name);
+
+    router.post('/register', jsonToDtoMiddlewareFactory(RegisterUserDto), (req, res) => registerUser(req, res, userRepository))
+    router.post('/login', jsonToDtoMiddlewareFactory(LoginDto), (req, res) => login(req, res, userRepository))
 }
--- Gateway/Authentication/src/index.ts
+++ Gateway/Authentication/src/index.ts
@@ -5,16 +5,42 @@ dotenv.config({path: '.env'});
 
 // Libs
 import express from 'express';
+import session from "express-session";
+let RedisStore = require("connect-redis")(session)
+
+// Redis for the session store
+const { createClient } = require("redis")
+let redisClient = createClient({
+    url: process.env.REDIS_URL,
+    legacyMode: true
+})
 
 // Custom files
 import {AppDataSource} from "./libraries/database";
 import {router, registerRoutes} from "./api/api";
 
-// Connect to database and run the app once the connection is established
-AppDataSource.initialize().then(() => {
+// Connect to databases and run the app once the connection is established
+Promise.all([
+    AppDataSource.initialize(),
+    redisClient.connect()
+]).then(() => {
     console.log("Database connected")
 
     const app = express();
+    app.set('trust proxy', 1);
+    app.use(session({
+        secret: process.env.SECRET,
+        name: 'sessionId',
+        saveUninitialized: false,
+        resave: false,
+        proxy: true,
+        cookie: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "prod",
+            maxAge: 365*24*60*60*1000, // 1 an
+        },
+        store: new RedisStore({ client: redisClient })
+    }))
 
     // Parse request body as json
     app.use(express.json());
