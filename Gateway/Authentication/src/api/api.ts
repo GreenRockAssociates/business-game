59c3815dde9d00ce081baa394fb38177cdb8daff
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-11 18:04:50 +0100

    Exposed change-password route to API

--- Gateway/Authentication/src/api/api.ts
+++ Gateway/Authentication/src/api/api.ts
@@ -1,20 +1,26 @@
 import express, {Router} from 'express';
 import {UserEntity} from "../entities/user.entity";
 import {DataSource, Repository} from "typeorm";
+
 import {RegisterUserDto} from "../dto/register-user.dto";
 import {LoginDto} from "../dto/login.dto";
-
+import {ChangePasswordDto} from "../dto/change-password.dto";
 import {jsonToDtoMiddlewareFactory} from "./middlewares/json-to-dto.middleware";
+
 import {registerUser} from "./routes/register.route";
 import {login} from "./routes/login.route";
 import {disconnect} from "./routes/disconnect.route";
+import {changePassword} from "./routes/change-password.route";
+import {guardAuthenticated} from "./middlewares/guard-authenticated.middleware";
+import {guardUnauthenticated} from "./middlewares/guard-unauthenticated.middleware";
 
 export const router = express.Router()
 
 export function registerRoutes(router: Router, dataSource: DataSource){
     const userRepository : Repository<UserEntity> = dataSource.getRepository(UserEntity.name);
 
-    router.post('/register', jsonToDtoMiddlewareFactory(RegisterUserDto), (req, res) => registerUser(req, res, userRepository))
-    router.post('/login', jsonToDtoMiddlewareFactory(LoginDto), (req, res) => login(req, res, userRepository))
-    router.post('/disconnect', disconnect)
+    router.post('/register', guardUnauthenticated, jsonToDtoMiddlewareFactory(RegisterUserDto), (req, res) => registerUser(req, res, userRepository));
+    router.post('/login', guardUnauthenticated, jsonToDtoMiddlewareFactory(LoginDto), (req, res) => login(req, res, userRepository));
+    router.post('/disconnect', disconnect);
+    router.post('/change-password', guardAuthenticated, jsonToDtoMiddlewareFactory(ChangePasswordDto), (req, res) => changePassword(req, res, userRepository));
 }
--- Gateway/Authentication/src/api/routes/change-password.route.ts
+++ Gateway/Authentication/src/api/routes/change-password.route.ts
@@ -7,19 +7,19 @@ import * as bcrypt from "bcrypt"
 export async function changePassword(req: Request, res: Response, repository: Repository<UserEntity>) {
     const dto = req.body as ChangePasswordDto;
     const user: UserEntity | null = await repository.findOneBy({
-        id: req.session.data.userId
+        id: req.session.data.userId // Assume `session.data` is not undefined since there should be a middleware taking care of that higher in the call chain
     });
 
     // If the user does not exist, return 401
     if (!user){
-        res.sendStatus(401)
+        res.sendStatus(401);
         return;
     }
 
     // If the password is invalid, return 401 as well
     // Don't leave any way to distinguish between wrong session or wrong password
     if (!await bcrypt.compare(dto.oldPassword, user.passwordHash) ){
-        res.sendStatus(401)
+        res.sendStatus(401);
         return;
     }
 
