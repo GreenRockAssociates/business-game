432e49c87eeb33dd31c74356c54b13459d8e0587
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-11 17:55:08 +0100

    Changed parameter name

--- Gateway/Authentication/src/api/middlewares/json-to-dto.middleware.ts
+++ Gateway/Authentication/src/api/middlewares/json-to-dto.middleware.ts
@@ -5,15 +5,15 @@ import {sanitize} from "class-sanitizer";
 import "reflect-metadata";
 
 /**
- * Returns a middleware that will parse req.body to a sanitized instance of the DTO given in {type}
+ * Returns a middleware that will parse req.body to a sanitized instance of the DTO given in {dtoType}
  *
  * If req.body is invalid, sends an error 400 as response
- * @param type The name of the DTO to parse to
+ * @param dtoType The name of the DTO to parse to
  * @param skipMissingProperties
  */
-export function jsonToDtoMiddlewareFactory(type: any, skipMissingProperties = false){
+export function jsonToDtoMiddlewareFactory(dtoType: any, skipMissingProperties = false){
     return async (req: Request, res: Response, next: NextFunction) => {
-        const dto = plainToInstance(type, req.body, { excludeExtraneousValues: true });
+        const dto = plainToInstance(dtoType, req.body, { excludeExtraneousValues: true });
 
         // Use try/catch block to be able to return from the function in case of error
         try {
