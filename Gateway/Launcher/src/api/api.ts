816800f07c8099df339f062dbc91d82a82e86ed3
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-29 16:45:33 +0100

    Exposed the new game route to API

--- Gateway/Launcher/src/api/api.ts
+++ Gateway/Launcher/src/api/api.ts
@@ -1,12 +1,15 @@
 import express, {Router} from 'express';
 import {DataSource, Repository} from "typeorm";
+import {GameEntity} from "../entities/game.entity";
 
 import {jsonToDtoMiddlewareFactory} from "./middlewares/json-to-dto.middleware";
-import {GetGameDetailRequestDto} from "../dto/get-game-detail-request.dto";
-import {getAllGames} from "./routes/get-all-games.route";
-import {GameEntity} from "../entities/game.entity";
-import {getGame} from "./routes/get-game.route";
 import {requestParamsToDtoMiddlewareFactory} from "./middlewares/request-params-to-dto.middleware";
+import {GetGameDetailRequestDto} from "../dto/get-game-detail-request.dto";
+import {CreateGameRequestDto} from "../specs/dto/create-game-request.dto";
+
+import {getAllGames} from "./routes/get-all-games.route";
+import {getGame} from "./routes/get-game.route";
+import {newGame} from "./routes/new-game.route";
 
 export const router = express.Router()
 
@@ -15,4 +18,5 @@ export function registerRoutes(router: Router, dataSource: DataSource){
 
     router.get("/games", (req, res) => getAllGames(req, res, gameRepository))
     router.get("/games/:gameId", requestParamsToDtoMiddlewareFactory(GetGameDetailRequestDto), (req, res) => getGame(req, res, gameRepository))
+    router.post("/new-game", jsonToDtoMiddlewareFactory(CreateGameRequestDto), (req, res) => newGame(req, res, gameRepository))
 }
