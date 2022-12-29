b35221e8145335662dcd035a4acfce772ff319bc
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-29 15:19:49 +0100

    Exposed get-all-games and get-game-details to the API

--- Gateway/Launcher/src/api/api.ts
+++ Gateway/Launcher/src/api/api.ts
@@ -2,8 +2,17 @@ import express, {Router} from 'express';
 import {DataSource, Repository} from "typeorm";
 
 import {jsonToDtoMiddlewareFactory} from "./middlewares/json-to-dto.middleware";
+import {GetGameDetailRequestDto} from "../dto/get-game-detail-request.dto";
+import {getAllGames} from "./routes/get-all-games.route";
+import {GameEntity} from "../entities/game.entity";
+import {getGame} from "./routes/get-game.route";
+import {requestParamsToDtoMiddlewareFactory} from "./middlewares/request-params-to-dto.middleware";
 
 export const router = express.Router()
 
 export function registerRoutes(router: Router, dataSource: DataSource){
+    const gameRepository: Repository<GameEntity> = dataSource.getRepository(GameEntity)
+
+    router.get("/games", (req, res) => getAllGames(req, res, gameRepository))
+    router.get("/games/:gameId", requestParamsToDtoMiddlewareFactory(GetGameDetailRequestDto), (req, res) => getGame(req, res, gameRepository))
 }
