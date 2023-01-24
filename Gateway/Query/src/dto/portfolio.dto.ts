25c91b0a1a24796e294f19cb1853c15d977aec98
EnzoFilangi <enzo.filangi@efrei.net>
2023-01-24 14:28:24 +0100

    Market DTO + fixed regex

--- Gateway/Query/src/dto/portfolio.dto.ts
+++ Gateway/Query/src/dto/portfolio.dto.ts
@@ -1,9 +1,12 @@
-import {Expose} from "class-transformer";
-import {IsArray, IsNumber, Matches} from "class-validator";
+import {Expose, Type} from "class-transformer";
+import {IsArray, IsNumber, Matches, ValidateNested} from "class-validator";
+import 'reflect-metadata';
 
 export class PortfolioDto {
     @Expose()
     @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => PortfolioEntryDto)
     portfolio: PortfolioEntryDto[]
 
     constructor(portfolio: PortfolioEntryDto[]) {
@@ -13,7 +16,7 @@ export class PortfolioDto {
 
 export class PortfolioEntryDto {
     @Expose()
-    @Matches(/[A-Z]{4,5}/) // Ensure the ticker has a valid format : 4 to 5 uppercase characters
+    @Matches(/^[A-Z]{4,5}$/) // Ensure the ticker has a valid format : 4 to 5 uppercase characters
     assetId: string
 
     @Expose()
