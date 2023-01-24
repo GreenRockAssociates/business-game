084a4592235678af33b48caba2cfc7d5e51a4ec4
EnzoFilangi <enzo.filangi@efrei.net>
2023-01-24 14:02:10 +0100

    Bank account and portfolio routes

---
+++ Gateway/Query/src/api/routes/player-state/get-portfolio.route.ts
@@ -0,0 +1,20 @@
+import {PlayerStateService} from "../../../libraries/player-state.service";
+import {Request, Response} from "express";
+import {instanceToPlain} from "class-transformer";
+import {AxiosError} from "axios";
+import {PortfolioDto} from "../../../dto/portfolio.dto";
+
+export function GetPortfolioRoute(playerStateService: PlayerStateService) {
+    return async (req: Request, res: Response) => {
+        try {
+            const portfolio: PortfolioDto = await playerStateService.getPortfolio(req.session);
+            res.json(instanceToPlain(portfolio));
+        } catch (e) {
+            if (e instanceof AxiosError){
+                res.sendStatus(e.response.status || 500);
+            } else {
+                res.sendStatus(500);
+            }
+        }
+    }
+}
