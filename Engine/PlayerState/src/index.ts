cf2f2c3f4f1c959a9cb9cb925d1e531b9f2c308a
EnzoFilangi <enzo.filangi@efrei.net>
2023-02-03 11:40:12 +0100

    Fixed player state service index

--- Engine/PlayerState/src/index.ts
+++ Engine/PlayerState/src/index.ts
@@ -1,21 +1,38 @@
-import {DataSource} from "typeorm"
-import "reflect-metadata";
-import {ValidationSubscriber} from "./subcribers/validation.subscriber";
-import { join } from "path";
-
+// Configure environment variables
+// Do this before imports so that all modules can use the environment variables
 import dotenv from 'dotenv';
 dotenv.config({path: '.env'});
 
-export function DataSourceFactory() {
-    return new DataSource({
-        type: "postgres",
-        url: process.env[`TYPEORM_URL`],
-        database: "game",
-        synchronize: true,
-        logging: false,
-        entities: [join(__dirname, '/**/**.entity{.ts,.js}')],
-        subscribers: [ValidationSubscriber],
-    })
-}
+// Libs
+import express, {Request, Response, NextFunction} from 'express';
 
-export const AppDataSource = DataSourceFactory()
+// Custom files
+import {AppDataSource} from "../../DataSource/src/index";
+import {router, registerRoutes} from "./api/api";
+
+// Connect to databases and run the app once the connection is established
+Promise.all([
+    AppDataSource.initialize(),
+]).then(() => {
+    console.log("Database connected")
+
+    const app = express();
+    app.set('trust proxy', 1);
+
+    // Parse request body as json
+    app.use(express.json());
+
+    // Configuration des routes
+    registerRoutes(router, AppDataSource)
+    app.use(process.env.SERVICE_URL_PREFIX, router)
+
+    // Custom error handling to avoid leaking stack trace
+    app.use((err: any, req: Request, res: Response, _: NextFunction) => {
+        res.sendStatus(err?.statusCode ?? 500);
+    })
+
+    const port = process.env.APP_PORT;
+    app.listen(port, () => {
+        console.log(`Player state service is running on port ${port}.`);
+    });
+})
