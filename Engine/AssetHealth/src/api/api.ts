2cc5b7a9db3a26b82f2a875804ba4423add24a6e
EnzoFilangi <enzo.filangi@efrei.net>
2023-02-04 13:41:13 +0100

    Service skeleton

--- Engine/AssetHealth/src/api/api.ts
+++ Engine/AssetHealth/src/api/api.ts
@@ -1,12 +1,5 @@
 import express, {Router} from 'express';
-//import {UserEntity} from "../entities/user.entity";
-import {DataSource, Repository} from "typeorm";
-
-import {NewsResponseDto} from "../dto/news-response.dto";
-
-//import {jsonToDtoMiddlewareFactory} from "./middlewares/json-to-dto.middleware";
-
-
+import {DataSource} from "typeorm";
 
 export const router = express.Router()
 
