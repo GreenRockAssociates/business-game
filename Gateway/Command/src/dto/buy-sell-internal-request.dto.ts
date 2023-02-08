40a368f1f7db8b83f127b12ba318094dd2588084
EnzoFilangi <enzo.filangi@efrei.net>
2023-02-08 12:18:22 +0100

    Fixed ticker regex in DTO to include smaller tickers

--- Gateway/Command/src/dto/buy-sell-internal-request.dto.ts
+++ Gateway/Command/src/dto/buy-sell-internal-request.dto.ts
@@ -7,7 +7,7 @@ export class BuySellInternalRequestDto {
     playerId: string
 
     @Expose()
-    @Matches(/^[A-Z]{4,5}$/) // Ensure the ticker has a valid format : 4 to 5 uppercase characters
+    @Matches(/^[A-Z]{1,5}$/) // Ensure the ticker has a valid format : 4 to 5 uppercase characters
     assetId: string
 
     @Expose()
