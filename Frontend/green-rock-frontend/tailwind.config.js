2c7d420648ec13e08dc939a61169c95028de2127
EnzoFilangi <enzo.filangi@efrei.net>
2023-02-23 10:02:15 +0100

    Added base colors to tailwind

--- Frontend/green-rock-frontend/tailwind.config.js
+++ Frontend/green-rock-frontend/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light': '#DFD5D5',
+        'dark': {
+          500: '#4E455E',
+          600: '#443D52',
+          700: '#3A3446',
+        }
+      },
+    },
   },
   plugins: [],
 }
