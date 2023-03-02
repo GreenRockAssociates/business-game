db40f5b7a10fb9f02413c7a3e51629221f998823
EnzoFilangi <enzo.filangi@efrei.net>
2023-03-02 21:53:01 +0100

    Portfolio page

--- Frontend/green-rock-frontend/src/app/pages/game/portfolio/portfolio.component.ts
+++ Frontend/green-rock-frontend/src/app/pages/game/portfolio/portfolio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {QueryService} from "../../../services/query-service/query.service";
+import {ActivatedRoute} from "@angular/router";
+import {AssetDetailDto} from "../../../interfaces/dto/asset-detail.dto";
+import {MarketService} from "../../../services/market-service/market.service";
 
 @Component({
   selector: 'app-portfolio',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  gameId: string | undefined;
+  portfolio: { asset: AssetDetailDto, quantity: number }[] = [];
+
+  constructor(
+    private queryService: QueryService,
+    private marketService: MarketService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.gameId = this.route.parent!.snapshot.params['gameId']; // Never null since we always refer to a game when navigating to this URL
+    if (this.gameId) {
+      this.queryService.getPortfolioWithAssetDetail(this.gameId).subscribe({
+        next: portfolio => this.portfolio = portfolio
+      })
+    }
   }
 
+  computePortfolioValue() {
+    return this.portfolio.reduce((total, current) => {
+      return total + (current.quantity * this.marketService.getAssetCurrentValue(current.asset.assetTicker))
+    }, 0);
+  }
 }
