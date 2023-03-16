61c8ef1e8e51cf261d2e9e1bec145a5a71b2c49b
EnzoFilangi <enzo.filangi@efrei.net>
2023-03-16 17:10:16 +0100

    Environment variables for production

--- Frontend/green-rock-frontend/src/environments/environment.prod.ts
+++ Frontend/green-rock-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  baseServerUrl: "//pfe.enzofilangi.fr/api", // Use "//" instead of "http://" so angular considers it a relative url and sends the CSRF tokens
+  authenticationService: "/auth",
+  launcherService: "/launcher",
+  queryService: "/query",
+  commandService: "/command",
+  realTimeUpdateService: "/real-time-update"
 };
