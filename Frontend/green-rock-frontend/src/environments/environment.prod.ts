30af109ccb84b5948ed17eddb36f1a8a9f7b1764
EnzoFilangi <enzo.filangi@efrei.net>
2023-03-31 17:36:50 +0200

    Changed frontend env files for local dev

--- Frontend/green-rock-frontend/src/environments/environment.prod.ts
+++ Frontend/green-rock-frontend/src/environments/environment.prod.ts
@@ -1,9 +1,17 @@
 export const environment = {
-  production: true,
-  baseServerUrl: "//pfe.enzofilangi.fr/api", // Use "//" instead of "http://" so angular considers it a relative url and sends the CSRF tokens
-  authenticationService: "/auth",
-  launcherService: "/launcher",
-  queryService: "/query",
-  commandService: "/command",
-  realTimeUpdateService: "/real-time-update"
+  // production: true,
+  // baseServerUrl: "//pfe.enzofilangi.fr/api", // Use "//" instead of "http://" so angular considers it a relative url and sends the CSRF tokens
+  // authenticationService: "/auth",
+  // launcherService: "/launcher",
+  // queryService: "/query",
+  // commandService: "/command",
+  // webSocketUrl: "ws://pfe.enzofilangi.fr/api/real-time-update"
+
+  production: false,
+  baseServerUrl: "//localhost", // Use "//" instead of "http://" so angular considers it a relative url and sends the CSRF tokens
+  authenticationService: ":30000/api/auth",
+  launcherService: ":30001/api/launcher",
+  queryService: ":30002/api/query",
+  commandService: ":30003/api/command",
+  webSocketUrl: "ws://localhost:30004"
 };
